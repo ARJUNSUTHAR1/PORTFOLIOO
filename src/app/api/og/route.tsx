d109ec129e9baa1ac747320e598417d31c059779
Arjun <arjunsuthar2112@gmail.com>
2026-03-01 16:52:59 +0530

    add the cloudinary image url in og section

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -3,46 +3,14 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export async function GET() {
-  // Fetch the og.png image from public folder and convert to base64
-  const ogImageBuffer = await fetch(
-    new URL("../../../../public/og.png", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-  
-  // Convert to base64 - Buffer is polyfilled in Next.js Edge runtime
-  const ogImageBase64 = Buffer.from(ogImageBuffer).toString("base64");
-  const ogImage = `data:image/png;base64,${ogImageBase64}`;
 
-  // const usdcBuffer = await fetch(new URL('../../../../public/usdc.svg', import.meta.url)).then(
-  //     (res) => res.arrayBuffer()
-  // );
-  // const usdcBase64 = Buffer.from(usdcBuffer).toString('base64');
-  // // ------------
-  // const usdtBuffer = await fetch(new URL('../../../../public/usdt.svg', import.meta.url)).then(
-  //     (res) => res.arrayBuffer()
-  // );
-  // const usdtBase64 = Buffer.from(usdtBuffer).toString('base64');
-  // // ------------
-
-  //   const logoBuffer = await fetch(
-  //     new URL("../../../assets/logo.png", import.meta.url)
-  //   ).then((res) => res.arrayBuffer());
-  //   const logoBase64 = Buffer.from(logoBuffer).toString("base64");
-  //   // ------------
-
-  //   // const usdc = `data:image/svg+xml;base64,${usdcBase64}`;
-  //   // const usdt = `data:image/svg+xml;base64,${usdtBase64}`;
-  //   const logo = `data:image/svg+xml;base64,${logoBase64}`;
-
-  //   const fontData = await fetch(
-  //     new URL("../../../fonts/neutral_sans_bold.woff2", import.meta.url)
-  //   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
       <div tw=" bg-[#09090b] text-white w-full p-4 h-full flex items-center justify-center flex-col">
         <img
           tw="flex items-center justify-center border-black w-[20%] mb-6 p-2"
-          src={ogImage}
+          src="https://res.cloudinary.com/dxjm0u3vj/image/upload/v1772364112/ChatGPT_Image_Mar_1_2026_01_31_17_PM_ejlmru.png"
         />
         <div tw="text-[3rem]">Arjun Suthar</div>
         <div tw="">Passionate Frontend Developer</div>
